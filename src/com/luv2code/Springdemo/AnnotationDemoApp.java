ee98a8c77d706ce0bc5f12da793e68454d6e6407
tunze <tunde4excel@gmail.com>
2021-01-22 16:46:56 +0100

    Dependency injection with spring annotations

---
+++ src/com/luv2code/Springdemo/AnnotationDemoApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.Springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+    public static void main(String[] args) {
+        //read spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //get the bean from spring container
+        Coach coach = context.getBean( "tennisCoach",Coach.class);
+
+        //call method to get the daily fortune
+        System.out.println(coach.getDailyFortune());
+
+        //call a method on the bean
+        System.out.println(coach.getDailyWorkout());
+
+        //close the context
+        context.close();
+    }
+
+}
---
+++ src/com/luv2code/Springdemo/FortuneService.java
@@ -0,0 +1,7 @@
+package com.luv2code.Springdemo;
+
+public interface FortuneService {
+
+
+    public String getFortune();
+}
